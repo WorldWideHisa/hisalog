0f3c724b1b5c03f25d3f5a121fbdc3b15e45d271
hisaro <worldwidehisa@gmail.com>
2024-06-05 09:34:46 +0900

    @nuxt/contentによるrouting実装

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@nuxt/content"],
-  srcDir: "articles/",
   content: {
-    sources: [path.join(__dirname, "content")],
-  },
+    sources: {
+      github: {
+        prefix: '/articles', // Prefix for routes used to query contents
+        driver: 'github', // Driver used to fetch contents (view unstorage documentation)
+        repo: "WorldWideHisa/wwh-blog-content",
+        branch: "main",
+        dir: "content/articles", // Directory where contents are located. It could be a subdirectory of the repository.
+        // Imagine you have a blog inside your content folder. You can set this option to `content/blog` with the prefix option to `/blog` to avoid conflicts with local files.
+      },
+    }
+  }
 })
