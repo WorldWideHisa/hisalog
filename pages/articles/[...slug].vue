b553e6d24d1cf36ae05dc726c267d63ca386aa6d
hisaro <worldwidehisa@gmail.com>
2024-06-10 08:06:08 +0900

    debugスクリプトをいれてみる

--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -3,3 +3,17 @@
     <ContentDoc />
   </main>
 </template>
+
+<script setup>
+import { useContent } from '@nuxt/content'
+
+// 現在のルートパラメータを取得
+const route = useRoute()
+const { slug } = route.params
+
+// コンテンツを取得
+const { data: article } = await useContent().findOne(slug)
+
+// 記事データを出力してデバッグ
+console.log(article)
+</script>
