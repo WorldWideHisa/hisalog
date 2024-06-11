86a3a02874b6b438cab90ee8da30a060c53cc331
hisaro <worldwidehisa@gmail.com>
2024-06-12 08:01:27 +0900

    動的に検索できるようにしてみる

--- pages/articles/[...slug].vue
+++ pages/articles/[...slug].vue
@@ -1,12 +1,23 @@
 <template>
-  <div>
-    <h1>Slug: {{ slug }}</h1>
+  <div v-if="article">
+    <ContentDoc :document="article" />
+  </div>
+  <div v-else>
+    <p>記事が見つかりませんでした。</p>
   </div>
 </template>
 
 <script setup>
+import { ref, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
+import { queryContent } from '@nuxt/content'
 
+const article = ref(null)
 const route = useRoute()
-const slug = route.params.slug
+
+onMounted(async () => {
+  const { slug } = route.params
+  const path = `/articles/${slug.join('/')}`
+  article.value = await queryContent(path).fetchOne()
+})
 </script>
