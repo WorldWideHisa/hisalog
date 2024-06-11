d702fa3f6c04eff121e419ad7e61af15b7987728
hisaro <worldwidehisa@gmail.com>
2024-06-11 22:51:34 +0900

    nitroを追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  nitro: {
+    preset: 'github-pages'
+  },
   devtools: { enabled: true },
   modules: ["@nuxt/content"],
   content: {
