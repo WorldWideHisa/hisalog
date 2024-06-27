4c951fede890355e2fe7ed6a2b10ed1ffada548e
hisaro <worldwidehisa@gmail.com>
2024-06-28 07:38:15 +0900

    preset消してみる

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { resolve } from "path";
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   nitro: {
-    preset: 'github-pages',
+    // preset: 'github-pages',
     prerender: {
       routes: [
         '/', // TOPページ
