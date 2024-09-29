15115a60531e560b96b55d4f62edc7c240acaaba
hisaro <worldwidehisa@gmail.com>
2024-09-29 23:46:12 +0900

    記事リストページ追加

---
+++ pages/articles.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>記事一覧</h1>
+    <ul>
+      <li v-for="article in articles" :key="article._path">
+        <nuxt-link :to="article._path">{{ article.title }}</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+// 記事データを取得
+const { data: articles } = await useAsyncData("articles", () =>
+  queryContent("/articles").find(),
+);
+</script>
